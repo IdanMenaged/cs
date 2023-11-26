403ec740d64eac4f5cfe8550b0dfd6d8529d2457
IdanMenaged <idanmenaged@gmail.com>
2023-11-26 18:12:51 +0200

    started networks peoj

--- cyber/networks/client.py
+++ cyber/networks/client.py
@@ -1,39 +1,22 @@
-"""
-Idan Menaged
-"""
-
 import socket
 
-IP = '0.0.0.0'
-PORT = 40
-MSG_LEN = 1024
+IP = '127.0.0.1'
+PORT = 1234
 
 
 def main():
-    try:
-        my_socket = initiate_client_socket(IP, PORT)
-
-        handle_user_input(my_socket)
-        my_socket.close()
-    except socket.error:
-        print("server is down")
+    init_client()
 
 
-def initiate_client_socket(ip, port):
+def init_client(ip=IP, port=PORT):
+    """
+    create a socket and connect to server
+    :return: socket
+    """
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_socket.connect((ip, port))
+
     return my_socket
-
-
-def handle_user_input(my_socket):
-    msg = input("please enter a request ")
-    while msg.upper() != 'EXIT':
-        my_socket.send(msg.encode())
-        data = my_socket.recv(MSG_LEN)
-        print("print bytes: ", data)
-        print("print string: ", data.decode())
-
-        msg = input("please enter a request ")
 
 
 if __name__ == '__main__':
