db75a0b72d34dcbd748e9f740bf1c0a48a9af382
idan menaged <IdanMenaged>
2023-12-01 15:51:54 +0200

    started networks 5

--- cyber/networks/server.py
+++ cyber/networks/server.py
@@ -1,13 +1,17 @@
 """
 Idan Menaged
 """
-
+import datetime
+import random
 import socket
+import time
 
 IP = '0.0.0.0'
 PORT = 1234
 MSG_LEN = 1024
 SIMULTANEOUS_CLIENTS = 1
+SERVER_NAME = "idan's amazing server"
+RAND_RANGE = (1, 11)
 
 
 def main():
@@ -56,6 +60,48 @@ def handle_single_client(client_socket):
         data = client_socket.recv(MSG_LEN)
 
     client_socket.close()
+
+
+def send_time(client_socket):
+    """
+    sends client the current date and time
+    :param client_socket: client socket
+    """
+    client_socket.send(str(datetime.datetime.now()).encode())
+
+
+def send_name(client_socket):
+    """
+    sends the name of the server
+    :param client_socket: client socket
+    """
+    client_socket.send(SERVER_NAME)
+
+
+def send_rand(client_socket):
+    """
+    sends a random number between 1 and 10
+    :param client_socket: client socket
+    """
+    client_socket.send(random.randrange(RAND_RANGE[0], RAND_RANGE[1]))
+
+
+def quit_client_connection(client_socket):
+    """
+    ends connection with client and waits for another connection
+    :param client_socket: client socket
+    """
+    client_socket.close()
+
+
+def exit_server(server_socket, client_socket):
+    """
+    ends connection with client and also terminate the server
+    :param server_socket: the socket handling the whole server
+    :param client_socket: the socket handling a specific connection
+    """
+    client_socket.close()
+    server_socket.close()
 
 
 if __name__ == '__main__':
