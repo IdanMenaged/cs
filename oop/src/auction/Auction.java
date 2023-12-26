364cc542f4c11650f7aa6dd8676ddae0202b7d63
IdanMenaged <idanmenaged@gmail.com>
2023-12-26 11:46:28 +0200

    oop auction

--- oop/src/auction/Auction.java
+++ oop/src/auction/Auction.java
@@ -1,5 +1,34 @@
 package auction;
 
 public class Auction {
-    private Item[] items;
+    private static final int MAX_ITEMS = 100;
+
+    private Item[] items; // all items
+    private int current; // current n of items
+
+    /**
+     * constructor
+     * sets items to an empty array of size MAX_ITEMS
+     * sets current to 0
+     */
+    public Auction() {
+        this.items = new Item[Auction.MAX_ITEMS];
+        this.current = 0;
+    }
+
+    /**
+     * add a new item
+     * @param item new item
+     * @return was action successful?
+     */
+    public boolean addItem(Item item) {
+        if (this.current >= Auction.MAX_ITEMS) {
+            return false;
+        }
+
+        this.items[this.current] = new Item(item);
+        this.current++;
+
+        return true;
+    }
 }
