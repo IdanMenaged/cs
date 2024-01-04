38f30a620616e2a93505ceeccbe33fe874f3dc31
IdanMenaged <idanmenaged@gmail.com>
2024-01-04 09:56:40 +0200

    how many nor finished

--- data_structures/src/queue/classes/QueueClasses.java
+++ data_structures/src/queue/classes/QueueClasses.java
@@ -8,11 +8,40 @@ Idan Menaged
 
 public class QueueClasses {
     public static void main(String[] args) {
-        Queue<Runner> q = new Queue<>();
+        Queue<Integer> q = new Queue<>();
         for (int i = 0; i < 3; i++) {
-            q.insert(new Runner("r" + i, i));
+            q.insert(i);
         }
-        System.out.println(bestRunner(q));
+        q.insert(1);
+        System.out.println(howMany(q));
+    }
+
+    /**
+     * count how many times a number appears in a q
+     * @param q queue of integers
+     * @param n number to count appearances of
+     * @return number of times n appears in q
+     */
+    private static int countAppearances(Queue<Integer> q, int n) {
+        // insert flag
+        q.insert(null);
+
+        int count = 0;
+
+        // go over q
+        while (q.head() != null) {
+            if (q.head() == n) {
+                count++;
+            }
+
+            // move to the back of the q
+            q.insert(q.remove());
+        }
+
+        // remove flag
+        q.remove();
+
+        return count;
     }
 
     /**
@@ -26,7 +55,7 @@ public class QueueClasses {
             return "";
         }
 
-        // insert null
+        // insert flag
         q.insert(null);
 
         // init best
@@ -43,9 +72,35 @@ public class QueueClasses {
             q.insert(q.remove());
         }
 
-        // remove null
+        // remove flag
         q.remove();
 
         return best.getName();
+    }
+
+    /**
+     * create a queue of pairs where each pair contains a number from the original queue and the number of times it
+     * appears in the original queue
+     * @param q queue of integers
+     * @return queue of pairs based on the rules above
+     */
+    public static Queue<Pair> howMany(Queue<Integer> q) {
+        // insert flag
+        q.insert(null);
+
+        // go over q
+        Queue<Pair> out = new Queue<>();
+        while (q.head() != null) {
+            Pair pair = new Pair(q.head(), countAppearances(q, q.head())); // FIXME: null appears twice
+            out.insert(pair);
+
+            // move to the back of the q
+            q.insert(q.remove());
+        }
+
+        // remove flag
+        q.remove();
+
+        return out;
     }
 }
