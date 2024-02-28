16fdf2fb575ba0e670dcf0abcb690034d09866df
IdanMenaged <idanmenaged@gmail.com>
2024-02-28 15:21:31 +0200

    smth smth i hate list summary

--- data_structures/src/ListSummary.java
+++ data_structures/src/ListSummary.java
@@ -4,11 +4,18 @@
 
 public class ListSummary {
     public static void main(String[] args) {
-        Node<String> lst = new Node<>("a",
-                new Node<>("b",
-                        new Node<>("c",
-                                new Node<>("b"))));
-        System.out.println(delAllSt(lst, "b"));
+        Node<Integer> lst = new Node<>(1,
+                new Node<>(2,
+                        new Node<>(3,
+                                new Node<>(-99,
+                                        new Node<>(3,
+                                                new Node<>(2,
+                                                        new Node<>(1,
+                                                                new Node<>(-99,
+                                                                        new Node<>(1,
+                                                                                new Node<>(3,
+                                                                                        new Node<>(2)))))))))));
+        System.out.println(consistentCount(lst));
     }
 
     /**
@@ -40,5 +47,68 @@ public class ListSummary {
         }
 
         return lst;
+    }
+
+    /**
+     * @param lst a list
+     * @return a new list in a reverse order
+     */
+    public static Node<Integer> reverse(Node<Integer> lst) {
+        // deal w/ first element
+        Node<Integer> out = new Node<>(lst.getValue());
+        lst=  lst.getNext();
+
+        // go over lst and each time create a new node and make it the head of out
+        while (lst != null) {
+            Node<Integer> temp = new Node<>(lst.getValue());
+            temp.setNext(out);
+            out = temp;
+
+            lst = lst.getNext();
+        }
+
+        return out;
+    }
+
+    /**
+     * a consistent sequence is one where it either goes up all the way or down all the way.
+     * each sequence in a list is separated by the number -99.
+     * @param lst a list
+     * @return n of consistent sequences in lst
+     */
+    public static int consistentCount(Node<Integer> lst) {
+        int count = 0;
+        while (lst != null) {
+            if (isIncreasing(lst)) {
+                count++;
+            }
+            else if (isDecreasing(lst)) {
+                count++;
+            }
+
+            while (lst != null && lst.getValue() != -99) {
+                lst = lst.getNext();
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean isIncreasing(Node<Integer> lst) {
+        while (lst.hasNext() && lst.getNext().getValue() != -99) {
+            if (lst.getNext().getValue() < lst.getValue()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDecreasing(Node<Integer> lst) {
+        while (lst.hasNext() && lst.getNext().getValue() != -99) {
+            if (lst.getNext().getValue() > lst.getValue()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
