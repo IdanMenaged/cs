d10d4c1c5f56509bfe066ac74a30a25801a7f3eb
IdanMenaged <idanmenaged@gmail.com>
2024-04-02 10:30:51 +0300

    removed commented code

--- data_structures/src/Pole.java
+++ data_structures/src/Pole.java
@@ -36,28 +36,6 @@ public class Pole
      * sorts the pole so that large rings are first
      */
     public void sort() {
-//        Node<Ring> pole = this.head;
-//        Node<Ring> largeRings = new Node<>(), smallRings = new Node<>();
-//        Node<Ring> lastLarge = largeRings, lastSmall = smallRings;
-//
-//        // go over pole and add to the right list
-//        while (pole != null) {
-//            if (pole.getValue().getSize().equals("L")) {
-//                lastLarge.setNext(new Node<>(pole.getValue()));
-//                lastLarge = lastLarge.getNext();
-//            }
-//            else {
-//                lastSmall.setNext(new Node<>(pole.getValue()));
-//                lastSmall = lastSmall.getNext();
-//            }
-//
-//            pole = pole.getNext();
-//        }
-//
-//        // construct joint list
-//        lastLarge.setNext(smallRings.getNext());
-//        this.head = largeRings.getNext();
-
         // divide into 2 poles
         Pole largeP = new Pole(), smallP = new Pole();
         while (!this.isEmpty()) {
