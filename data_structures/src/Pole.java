1c9f14af2e66da9ca8b8b84cab1ca0a32a736c9b
IdanMenaged <idanmenaged@gmail.com>
2024-04-02 10:30:21 +0300

    new structures tested

--- data_structures/src/Pole.java
+++ data_structures/src/Pole.java
@@ -36,27 +36,47 @@ public class Pole
      * sorts the pole so that large rings are first
      */
     public void sort() {
-        Node<Ring> pole = this.head;
-        Node<Ring> largeRings = new Node<>(), smallRings = new Node<>();
-        Node<Ring> lastLarge = largeRings, lastSmall = smallRings;
+//        Node<Ring> pole = this.head;
+//        Node<Ring> largeRings = new Node<>(), smallRings = new Node<>();
+//        Node<Ring> lastLarge = largeRings, lastSmall = smallRings;
+//
+//        // go over pole and add to the right list
+//        while (pole != null) {
+//            if (pole.getValue().getSize().equals("L")) {
+//                lastLarge.setNext(new Node<>(pole.getValue()));
+//                lastLarge = lastLarge.getNext();
+//            }
+//            else {
+//                lastSmall.setNext(new Node<>(pole.getValue()));
+//                lastSmall = lastSmall.getNext();
+//            }
+//
+//            pole = pole.getNext();
+//        }
+//
+//        // construct joint list
+//        lastLarge.setNext(smallRings.getNext());
+//        this.head = largeRings.getNext();
 
-        // go over pole and add to the right list
-        while (pole != null) {
-            if (pole.getValue().getSize().equals("L")) {
-                lastLarge.setNext(new Node<>(pole.getValue()));
-                lastLarge = lastLarge.getNext();
+        // divide into 2 poles
+        Pole largeP = new Pole(), smallP = new Pole();
+        while (!this.isEmpty()) {
+            Ring r = this.remove();
+            if (r.getSize().equals("L")) {
+                largeP.add(r);
             }
             else {
-                lastSmall.setNext(new Node<>(pole.getValue()));
-                lastSmall = lastSmall.getNext();
+                smallP.add(r);
             }
-
-            pole = pole.getNext();
         }
 
-        // construct joint list
-        lastLarge.setNext(smallRings.getNext());
-        this.head = largeRings.getNext();
+        // join poles
+        while (!largeP.isEmpty()) {
+            this.add(largeP.remove());
+        }
+        while (!smallP.isEmpty()) {
+            this.add(smallP.remove());
+        }
     }
     
     // toString
